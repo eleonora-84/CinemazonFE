9116069efb8f929113ae767051fd436967347cb7
EleonoraPisacane <eleonora.pisacane@yahoo.it>
2023-03-02 00:33:36 +0100

    css admin home

--- src/component/admin/AdminHome.jsx
+++ src/component/admin/AdminHome.jsx
@@ -1,23 +1,29 @@
 import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 
 const AdminHome = () => {
   return (
     <div className="adminHome">
-      <Link to="/addmovies" className="adminItem">
-        Aggiungi Film
-      </Link>
-      <strong>||</strong>
-      <Link to="/deletemovies" className="adminItem">
-        Cancella Film
-      </Link>
-      <strong className="h1">||</strong>
-      <Link to="/addshows" className="adminItem">
-        Aggiungi Spettacoli
-      </Link>
-      <strong>||</strong>
-      <Link to="/deleteshows" className="adminItem">
-        Cancella Spettacoli
-      </Link>
+      <Button variant="light" className="adminBtn">
+        <Link to="/addmovies" className="adminItem">
+          Aggiungi Film
+        </Link>
+      </Button>
+      <Button variant="light" className="adminBtn">
+        <Link to="/deletemovies" className="adminItem">
+          Cancella Film
+        </Link>
+      </Button>
+      <Button variant="light" className="adminBtn">
+        <Link to="/addshows" className="adminItem">
+          Aggiungi Spettacoli
+        </Link>
+      </Button>      
+      <Button variant="light" className="adminBtn">
+        <Link to="/deleteshows" className="adminItem">
+          Cancella Spettacoli
+        </Link>
+      </Button>
     </div>
   );
 };
